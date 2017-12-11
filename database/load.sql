bbcf5548ecbe3878b658b201ec1319cd76827fee
Jack Siman <jack@Jacks-MacBook-Pro-7.local>
2017-12-11 12:19:13 -0500

    -Updated database files to include degree tables

--- database/load.sql
+++ database/load.sql
@@ -1,3 +1,5 @@
-\COPY School(name,tuition,size,rank) FROM 'data/school.csv' WITH DELIMITER ',' NULL '' CSV
-\COPY City(name,state,latitude,longitude,population,medianResidentAge,HSDegree,medianIncome,belowPovertyLine,foreignBorn,crimeIndex) FROM 'data/city.csv' WITH DELIMITER ',' NULL '' CSV
+\COPY School(name,size,tuition,rank) FROM 'data/school.csv' WITH DELIMITER ',' NULL '' CSV
+\COPY City(name,state,latitude,longitude,population,medianResidentAge,HSDegree,medianIncome,belowPovertyLine,foreignBorn,crimeIndex) FROM 'data/cities.csv' WITH DELIMITER ',' NULL '' CSV
 \COPY Location(school_name,city_name,state) FROM 'data/location.csv' WITH DELIMITER ',' NULL '' CSV
+\COPY Degree(name,start_salary,tenyear_salary) FROM 'data/degrees.csv' WITH DELIMITER ',' NULL '' CSV
+\COPY Offering(school_name,degree_name) FROM 'data/offering.csv' WITH DELIMITER ',' NULL '' CSV
